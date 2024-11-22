691d9927170b6e1237443d3a294fd5b31dfa65d7
pptomczyk <p.tomczyk231@gmail.com>
2024-11-22 14:32:24 +0100

    Spritesv2

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -3,40 +3,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private Vector2 movement;
-    private Rigidbody2D rb;
+    private Vector3 movement;
     private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    public bool canMove;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
        sprite = GetComponent<SpriteRenderer>();
+       canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey("d")){
-            movement = new Vector2(5,0);
+            movement = new Vector3(3,0,0);
             transform.localRotation = Quaternion.Euler(0, 0, 0);
 
         }
         if (Input.GetKey("a")){
-            movement = new Vector2(-5,0);
+            movement = new Vector3(-3,0,0);
             transform.localRotation = Quaternion.Euler(0, 0, 180);
 
         }
         if (Input.GetKey("w")){
-            movement = new Vector2(0,5);
+            movement = new Vector3(0,3,0);
             transform.localRotation = Quaternion.Euler(0, 0, 90);
 
         }
         if (Input.GetKey("s")){
-            movement = new Vector2(0,-5);
+            movement = new Vector3(0,-3,0);
             transform.localRotation = Quaternion.Euler(0, 0, 270);
 
         }
-        rb.MovePosition(rb.position+movement * Time.fixedDeltaTime);
+        rb.MovePosition(transform.position+movement * Time.fixedDeltaTime);
+    
+        
     }
 }
