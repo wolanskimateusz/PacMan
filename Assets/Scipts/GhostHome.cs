ddc5ced6895b0d9d78fd0e8273a79ff6d3880e61
wolanski_mateusz <94111142+wolanskimateusz@users.noreply.github.com>
2024-12-05 21:57:16 +0100

    Update GhostHome.cs

--- Assets/Scipts/GhostHome.cs
+++ Assets/Scipts/GhostHome.cs
@@ -11,4 +11,21 @@ public class GhostHome : MonoBehaviour
         this.ghost = GetComponent<Ghost>();
         this.enabled = false;
     }
+
+    public void Enable()
+    {
+        Enable(this.duration);
+
+    }
+    public void Enable(float duration)
+    {
+        this.enabled = true;
+        CancelInvoke();
+        Invoke(nameof(Disable),duration);
+    }
+    public void Disable()
+    {
+        this.enabled = false;
+        CancelInvoke();
+    }
 }
